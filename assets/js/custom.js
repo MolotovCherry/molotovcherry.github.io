d0d94172025f0a85c96dc980c5423d4e822b7cf8
Cherryleafroad <13651622+cherryleafroad@users.noreply.github.com>
2022-03-24 10:26:25 -0700

    Code block

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,31 +6,33 @@ function collapse_code_blocks() {
     // top element
     var code_blocks = [...document.querySelectorAll("pre.highlight")].map(x => x.parentElement.parentElement);
     
-    
-    
-    for (block in code_blocks) {
+    for (block of code_blocks) {
         var elem = document.createElement("button");
         elem.classList.add("collapsible");
         
+        var classNames = block.className.split(' ');
+        
         var language;
         var language_upper;
-        block.classList.forEach(
-            function(cls) {
-                if (cls.startsWith("language-")) {
-                    language = cls.substring(cls.indexOf('-') + 1);
-                    language_upper = language.split("");
-                    language_upper[0] = language[0].toUpperCase();
-                    language_upper = language_upper.join("");
-                }
+        classNames.forEach(name => {
+            if (name.startsWith("language-")) {
+                language = name.substring(name.indexOf('-') + 1);
+                language_upper = language.split("");
+                language_upper[0] = language[0].toUpperCase();
+                language_upper = language_upper.join("");
             }
-        );
+        });
         
         var icon = document.createElement("i");
         icon.classList.add("devicon-" + language + "-plain");
+        icon.classList.add("lang-icon");
         elem.appendChild(icon);
         
+        var div = document.createElement("div");
         var text = document.createTextNode(" " + language_upper);
-        elem.appendChild(text);
+        div.classList.add("lang-label");
+        div.appendChild(text);
+        elem.appendChild(div);
         
         block.parentNode.insertBefore(elem, block);
     }
