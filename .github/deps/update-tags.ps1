6b5ab78c8dbb5edade2227e59046e136da8e4dd6
Cherryleafroad <13651622+cherryleafroad@users.noreply.github.com>
2022-03-30 14:16:04 -0700

    Author pages

--- .github/deps/update-tags.ps1
+++ .github/deps/update-tags.ps1
@@ -1,25 +1,31 @@
 $tagsDir = "tag"
 $categoryDir = "category"
+$authorDir = "author"
+$author2Dir = "_authors"
 $postsDir = "_posts"
 $global:changed = $false
 $global:addedTags = @()
 $global:deletedTags = @()
 $global:addedCategories = @()
 $global:deletedCategories = @()
+$global:addedAuthor = @()
+$global:deletedAuthor = @()
 
 function Set-OutputVariable($Name, $Value) {
     Write-Host "::set-output name=$Name::$Value"
 }
 
-function Update($Posts, $TagList, $CategoryList) {
+function Update($Posts, $TagList, $CategoryList, $AuthorList, $AuthorList2) {
     $tags = @()
     $categories = @()
+    $author = @()
     foreach($post in $Posts) {
         $file = Get-Content -raw $post
 
         $pattern = "---(?s)(.*?)---"
         $tagReg = "tags: (.*)"
         $categoryReg = "category: (.*)"
+        $authorReg = "author: (.*)"
 
         $marker = [regex]::Match($file,$pattern).Groups[1].Value
         $lines = $marker.Split(
@@ -31,11 +37,15 @@ function Update($Posts, $TagList, $CategoryList) {
             foreach($line in $lines) {
                 $tagsFiltered = [regex]::Match($line, $tagReg).Groups[1]
                 $categoriesFiltered = [regex]::Match($line, $categoryReg).Groups[1]
+                $authorFiltered = [regex]::Match($line, $authorReg).Groups[1]
                 if($tagsFiltered.Length -gt 0) {
                     $tags += $tagsFiltered -split ' ' | ForEach-Object { $_.Trim() }
                 }
                 if($categoriesFiltered.Length -gt 0) {
                     $categories += $categoriesFiltered -split ' ' | ForEach-Object { $_.Trim() }
+                }
+                if($authorFiltered.Length -gt 0) {
+                    $author += $authorFiltered -split ' ' | ForEach-Object { $_.Trim() }
                 }
             }
         }
@@ -44,11 +54,16 @@ function Update($Posts, $TagList, $CategoryList) {
     # remove all duplicates
     $tags = $tags | Get-Unique
     $categories = $categories | Get-Unique
+    $author = $author | Get-Unique
 
     $newTags = $tags | Where-Object {$TagList -NotContains $_}
     $deletedTags = $TagList | Where-Object {$tags -NotContains $_}
     $newCategories = $categories | Where-Object {$CategoryList -NotContains $_}
     $deletedCategories = $CategoryList | Where-Object {$categories -NotContains $_}
+    $newAuthor = $author | Where-Object {$AuthorList -NotContains $_}
+    $deletedAuthor = $AuthorList | Where-Object {$author -NotContains $_}
+    $newAuthor2 = $author | Where-Object {$AuthorList2 -NotContains $_}
+    $deletedAuthor2 = $AuthorList2 | Where-Object {$author -NotContains $_}
 
     if ($newTags.Length -gt 0) {
         Write-Host "Added tags -> $newTags"
@@ -73,6 +88,21 @@ function Update($Posts, $TagList, $CategoryList) {
         $global:changed = $true
         $global:deletedCategories += $deletedCategories
         $deletedCategories | ForEach-Object { Remove-Item -Path "$categoryDir\$_.md" }
+    }
+    if ($newAuthor.Length -gt 0 -or $newAuthor2.Length -gt 0) {
+        $list = ($newAuthor + $newAuthor2) | Get-Unique
+        Write-Host "Added author -> $list"
+        $global:changed = $true
+        $global:addedAuthor += $list
+        CreateAuthor $newAuthor $newAuthor2
+    }
+    if ($deletedAuthor.Length -gt 0 -or $deletedAuthor2 -gt 0) {
+        $list = ($deletedAuthor + $deletedAuthor2) | Get-Unique
+        Write-Host "Deleted author -> $list"
+        $global:changed = $true
+        $global:deletedAuthor += $list
+        $deletedAuthor | ForEach-Object { Remove-Item -Path "$authorDir\$_.md" }
+        $deletedAuthor2 | ForEach-Object { Remove-Item -Path "$author2Dir\$_.md" }
     }
 }
 
@@ -116,18 +146,61 @@ robots: noindex
     }
 }
 
+function CreateAuthor($generatedAuthor, $generatedAuthor2) {
+    if (!(Test-Path $authorDir)) {
+        New-Item $authorDir -ItemType Directory | Out-Null
+    }
+    if (!(Test-Path $author2Dir)) {
+        New-Item $author2Dir -ItemType Directory | Out-Null
+    }
+
+    foreach($author in $generatedAuthor) {
+        $content = @"
+---
+layout: author-page
+title: "Author: $author"
+author: $author
+robots: noindex
+---
+"@
+
+        $author = $author.ToLower()
+        $content | Out-File -FilePath "$authorDir\$author.md"
+    }
+
+    foreach($author in $generatedAuthor2) {
+        $content = @"
+---
+name: $author
+username: $author
+---
+"@
+
+        $author = $author.ToLower()
+        $content | Out-File -FilePath "$author2Dir\$author.md"
+    }
+}
+
 
 $posts = Get-ChildItem "$postsDir" | ForEach-Object {$_.FullName}
 $tags = @()
 $categories = @()
+$author = @()
+$author2 = @()
 if (Test-Path $tagsDir) {
     $tags += Get-ChildItem $tagsDir | foreach-object {(Split-Path -leaf $_) -replace ".md",""}
 }
 if (Test-Path $categoryDir) {
     $categories += Get-ChildItem $categoryDir | foreach-object {(Split-Path -leaf $_) -replace ".md",""}
 }
+if (Test-Path $authorDir) {
+    $author += Get-ChildItem $authorDir | foreach-object {(Split-Path -leaf $_) -replace ".md",""}
+}
+if (Test-Path $author2Dir) {
+    $author2 += Get-ChildItem $author2Dir | foreach-object {(Split-Path -leaf $_) -replace ".md",""}
+}
 
-Update -Posts $posts -TagList $tags -CategoryList $categories
+Update -Posts $posts -TagList $tags -CategoryList $categories -AuthorList $author -AuthorList2 $author2
 
 # notify runner of status
 Set-OutputVariable -Name "changed" -Value "$global:changed"
@@ -143,5 +216,11 @@ if ($global:changed) {
     }
     if ($global:deletedCategories.Length -gt 0) {
         Set-OutputVariable -Name "deleted-categories" -Value "$global:deletedCategories"
+    }
+    if ($global:addedAuthor.Length -gt 0) {
+        Set-OutputVariable -Name "new-author" -Value "$global:addedAuthor"
+    }
+    if ($global:deletedAuthor.Length -gt 0) {
+        Set-OutputVariable -Name "deleted-author" -Value "$global:deletedAuthor"
     }
 }
---
+++ _authors/cherryleafroad.md
@@ -0,0 +1,8 @@
+---
+name: Cherryleafroad
+username: Cherryleafroad
+---
+
+I'm a software engineer specializing in Rust\*, Python, C#, JavaScript, Android, Kotlin, and a bit of Unity game dev. My skillset includes making Android apps, systems programming with Rust\*, SQLite database (Android Room for example), game dev using Unity (and a bit of 3d modeling thrown in there), Qt (especially PyQt), JNI / Kotlin, HTML+CSS+Javascript.
+
+\* I have an upper intermediate to advanced knowledge of Rust, including advanced things such as unsafe code, async (like Tokio), and even dynamic source code generation using procedural macros (syn).
